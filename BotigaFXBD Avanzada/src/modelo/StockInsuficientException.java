d684dad2711e41411fcd2ba50e90898c320b2111
Izar Mula Soria <izar19mula97@gmail.com>
2018-03-20 15:46:35 +0100

    ffgfg

---
+++ BotigaFXBD Avanzada/src/modelo/StockInsuficientException.java
@@ -0,0 +1,21 @@
+package modelo;
+
+@SuppressWarnings("serial")
+public class StockInsuficientException extends Exception {
+
+	
+	
+
+	public StockInsuficientException() {
+		super();
+	
+	}
+
+	public StockInsuficientException(String message) {
+		super(message);
+		
+	}
+	
+	
+
+}
