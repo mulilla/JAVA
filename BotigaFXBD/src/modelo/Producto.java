eb49363b9ef72843562d6f779c19d8bb021e1e5b
Izar Mula Soria <izar19mula97@gmail.com>
2018-03-20 15:49:09 +0100

    jhuh

---
+++ BotigaFXBD/src/modelo/Producto.java
@@ -0,0 +1,131 @@
+package modelo;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import modelo.StockInsuficientException;
+
+public abstract class Producto implements Serializable {
+	
+	 //Parametros de clase
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String id;
+	private String nom;
+	private double preu;
+	private int stock;
+	private LocalDate fecha_inicio;
+	private LocalDate fecha_final;
+	private int id_proveedor;
+	
+	//Contructores
+	
+	public Producto() {
+		this("","",0.0,0,null,null,1);
+			
+	}
+	public Producto(String id, String nom, double preu, int stock,LocalDate fecha_inicio,LocalDate fecha_final,int id_proveedor) {
+		this.id = id;
+		this.nom = nom;
+		this.preu = preu;
+		this.stock=stock;
+		this.fecha_inicio=fecha_inicio;
+		this.fecha_final=fecha_final;
+		this.id_proveedor=id_proveedor;
+	}
+	
+	
+	//Getters and Setters
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public double getPreu() {
+		return preu;
+	}
+	public void setPreu(double preu) {
+		this.preu = preu;
+	}
+	
+	
+	public int getStock() {
+		return stock;
+	}
+	
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	
+	
+	public void ponStock(int stock) {
+		
+		this.stock+=stock;
+		
+	}
+	
+	public LocalDate getFecha_inicio() {
+		return fecha_inicio;
+	}
+	public void setFecha_inicio(LocalDate fecha_inicio) {
+		this.fecha_inicio = fecha_inicio;
+	}
+	public LocalDate getFecha_final() {
+		return fecha_final;
+	}
+	public void setFecha_final(LocalDate fecha_final) {
+		this.fecha_final = fecha_final;
+	}
+	
+	
+	public int getId_proveedor() {
+		return id_proveedor;
+	}
+	public void setId_proveedor(int id_proveedor) {
+		this.id_proveedor = id_proveedor;
+	}
+	public void quitarStock(int stock) throws StockInsuficientException {
+		if(this.stock<stock) {
+			StockInsuficientException error= new StockInsuficientException("No hay suficiente Stock");
+			throw error;
+		}else {
+			this.stock-=stock;
+		}
+		
+	}
+	//Metodo que compara el nombre del producto instanciado con otro producto pasado por parametro
+	
+
+	
+	public boolean equals(Producto producto) {
+		return this.getNom().equals(producto.getNom());
+		
+		
+	}
+	
+	
+	
+	
+	
+	//Imprime los parametros de la clase productos.
+	public void imprimir() {
+		
+		System.out.println("ID: "+id+" Nombre: "+nom+" Precio: "+preu+"Stock: "+stock+" Fecha Inicio: "+fecha_inicio+" Fecha_final: "+fecha_final+" Id proveedor: "+id_proveedor);
+	}
+	
+	
+	
+	
+
+}
